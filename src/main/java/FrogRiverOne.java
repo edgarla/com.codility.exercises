21eed26313e56ab705997cd8262f24174881d6c5
edgarloraariza <edgar.lora.ariza@gmail.com>
2018-09-03 11:10:22 -0500

    [EL] add FrogRiverOne

---
+++ src/main/java/FrogRiverOne.java
@@ -0,0 +1,11 @@
+public class FrogRiverOne {
+
+    private static int solution(int X, int[] A) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] A1 = {1, 3, 1, 4, 2, 3, 5, 4};
+        System.out.println(solution(5, A1));
+    }
+}
