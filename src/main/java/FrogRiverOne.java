b897ccdc676a0c5774bd24d272c3246fc0b6de46
edgarloraariza <edgar.lora.ariza@gmail.com>
2018-09-03 15:02:28 -0500

    [EL] complete FrogRiverOne

--- src/main/java/FrogRiverOne.java
+++ src/main/java/FrogRiverOne.java
@@ -1,7 +1,24 @@
+import java.util.HashSet;
+
 public class FrogRiverOne {
 
     private static int solution(int X, int[] A) {
-        return 0;
+        HashSet<Integer> numbers = new HashSet<>();
+        int sumX = 0;
+        int sumA = 0;
+        for (int i = 1; i <= X; i++) {
+            sumX += i;
+        }
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] <= X && !numbers.contains(A[i])) {
+                numbers.add(A[i]);
+                sumA += A[i];
+                if (sumA == sumX){
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
