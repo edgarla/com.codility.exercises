0a7ff51094c8c26bbbcb300919bdb92cb21f292a
Edgar Lora <edgar.loraariza@wnco.com>
2018-09-03 16:44:36 -0500

    [EL] add MissingInteger

---
+++ src/main/java/MissingInteger.java
@@ -0,0 +1,11 @@
+public class MissingInteger {
+
+    private static int solution(int[] A) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] A1 = {1, 3, 6, 4, 1, 2};
+        System.out.println(solution(A1));
+    }
+}
